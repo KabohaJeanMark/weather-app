6605205cd7518c08aed52efeaf7db8ef4fd1f80f
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 21:31:42 +0300

    Add a method to convert temp to degrees celcius

--- src/index.js
+++ src/index.js
@@ -6,19 +6,27 @@ async function getWeather(city) {
 
 const displayWeatherInfo = document.getElementById('displayWeatherCard');
 
+const tempDiv = document.getElementById('tempInDegrees');
+
+function convertTempToCelsius(absoluteTemp) {
+  tempDiv.textContent = '';
+  const celciusTemp = absoluteTemp - 273;
+  tempDiv.textContent = `The temperature in degrees Celsius is ${celciusTemp}`;
+}
+
 function display(data) {
   const absoluteTemp = data.main.temp;
-  console.log(absoluteTemp);
   const mainWeather = data.weather[0].main;
-  console.log(mainWeather);
   const weatherDescription = data.weather[0].description;
-  console.log(weatherDescription);
   const div = document.createElement('div');
   const h3 = document.createElement('h3');
   h3.textContent = mainWeather;
   const p1 = document.createElement('p');
   p1.textContent = `${weatherDescription} with an Absolute temp in Kelvin of ${absoluteTemp}`;
-  div.append(h3, p1);
+  const celsiusBtn = document.createElement('button');
+  celsiusBtn.textContent = 'Temp in degrees Celsius';
+  celsiusBtn.addEventListener('click', convertTempToCelsius(absoluteTemp));
+  div.append(h3, p1, celsiusBtn);
   displayWeatherInfo.textContent = '';
   displayWeatherInfo.appendChild(div);
 };
