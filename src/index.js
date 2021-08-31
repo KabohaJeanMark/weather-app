d9f9a1f65122e8d5bab088b7f014a870aa337990
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 20:32:26 +0300

    Add npm run start command

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ let weather;
 async function getWeather(city) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
   const data = await response.json();
-  // console.log(data);
   const temp = data.main.temp - 273.15;
   console.log(temp);
   const mainWeather = data.weather[0].main;
