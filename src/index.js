02ddbf0625d66f9d23d9ce851c65f46a01d1c6f7
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 23:04:17 +0300

    Refactor to put the getWeather async function in its own module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import { convertTempToCelsius, convertTempToFahrenheit } from './tempMethods';
-
-const getWeather = async (city) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
-
-  if (response.status !== 200) {
-    throw new Error('Please enter full city name. Cannot fetch the data');
-  }
-  const data = await response.json();
-  return data;
-};
+import getWeather from './fetchWeather';
 
 const displayWeatherInfo = document.getElementById('displayWeatherCard');
 
