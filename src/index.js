7e4a860f6ec0f65d433a2d86d4172f829699d6c1
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 23:13:26 +0300

    Complete refactoring and separating code concerns into different JS modules

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
-import { convertTempToCelsius, convertTempToFahrenheit } from './tempMethods';
 import getWeather from './fetchWeather';
-
-const displayWeatherInfo = document.getElementById('displayWeatherCard');
-
-const display = (data) => {
-  const absoluteTemp = data.main.temp;
-  const mainWeather = data.weather[0].main;
-  const weatherDescription = data.weather[0].description;
-  const div = document.createElement('div');
-  const h3 = document.createElement('h3');
-  h3.textContent = mainWeather;
-  const p1 = document.createElement('p');
-  p1.textContent = `${weatherDescription} with an Absolute temp in Kelvin of ${absoluteTemp}`;
-  const celsiusBtn = document.createElement('button');
-  celsiusBtn.textContent = 'Temp in degrees Celsius';
-  celsiusBtn.classList.add('btn', 'btn-warning', 'm-1');
-  celsiusBtn.addEventListener('click', () => {
-    convertTempToCelsius(absoluteTemp);
-  });
-  const fahrenheitBtn = document.createElement('button');
-  fahrenheitBtn.textContent = 'Temp in degrees Fahrenheit';
-  fahrenheitBtn.classList.add('btn', 'btn-primary', 'm-1');
-  fahrenheitBtn.addEventListener('click', () => {
-    convertTempToFahrenheit(absoluteTemp);
-  });
-  div.append(h3, p1, celsiusBtn, fahrenheitBtn);
-  displayWeatherInfo.textContent = '';
-  displayWeatherInfo.appendChild(div);
-};
-
-const displayError = (errorMessage) => {
-  displayWeatherInfo.textContent = errorMessage;
-};
+import { display, displayError } from './view';
 
 const checkWeatherForm = document.getElementById('cityWeatherForm');
 checkWeatherForm.addEventListener('submit', (e) => {
