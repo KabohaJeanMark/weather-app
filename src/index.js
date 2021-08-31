1234ff8d410cbba1d1e9aff4965f5fe39cd072f0
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 19:35:25 +0300

    Reformat to use response and data

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ console.log('Hi testing');
 let weather;
 
 async function getWeather(city) {
-  const cityWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
-  const response = await cityWeather.json();
-  weather = response.weather;
-  console.log(weather);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
+  const data = await response.json();
+  console.log(data);
 };
 
 getWeather('Kampala');
