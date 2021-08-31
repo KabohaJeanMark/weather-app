15d03b63a269f920c587e8fc3ef46dceff1336a9
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 17:50:23 +0300

    Refactor method to shift from .then to instead use await

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ let weather;
 
 async function getWeather(city) {
   const cityWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
-  cityWeather.json().then(function(response){
-    weather = response.weather;
-    console.log(weather);
-  })
-
-}
+  const response = await cityWeather.json();
+  weather = response.weather;
+  console.log(weather);
+};
 
 getWeather('Kampala');
