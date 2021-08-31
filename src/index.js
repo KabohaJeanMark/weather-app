5a81313af07095b1aba50296e949a5fcd01ad5fa
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-08-31 22:45:27 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-async function getWeather(city) {
+const getWeather = async (city) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a907874d0e5af1a38cfac90720e9f08`);
 
   if (response.status !== 200) {
-    throw new Error('Please enter full city name. Cannot fetch the data')
+    throw new Error('Please enter full city name. Cannot fetch the data');
   }
   const data = await response.json();
   return data;
@@ -21,7 +21,7 @@ function convertTempToCelsius(absoluteTemp) {
 function convertTempToFahrenheit(absoluteTemp) {
   tempDiv.textContent = '';
   const celciusTemp = (absoluteTemp - 273.15);
-  const fahrenheitTemp = ((celciusTemp * 9/5) + 32).toFixed(2);
+  const fahrenheitTemp = ((celciusTemp * (9 / 5)) + 32).toFixed(2);
   tempDiv.textContent = `The temperature in degrees Fahrenheit is ${fahrenheitTemp}`;
 }
 
@@ -36,20 +36,20 @@ function display(data) {
   p1.textContent = `${weatherDescription} with an Absolute temp in Kelvin of ${absoluteTemp}`;
   const celsiusBtn = document.createElement('button');
   celsiusBtn.textContent = 'Temp in degrees Celsius';
-  celsiusBtn.classList.add('btn','btn-warning','m-1');
+  celsiusBtn.classList.add('btn', 'btn-warning', 'm-1');
   celsiusBtn.addEventListener('click', () => {
     convertTempToCelsius(absoluteTemp);
   });
   const fahrenheitBtn = document.createElement('button');
   fahrenheitBtn.textContent = 'Temp in degrees Fahrenheit';
-  fahrenheitBtn.classList.add('btn','btn-primary','m-1');
+  fahrenheitBtn.classList.add('btn', 'btn-primary', 'm-1');
   fahrenheitBtn.addEventListener('click', () => {
     convertTempToFahrenheit(absoluteTemp);
   });
   div.append(h3, p1, celsiusBtn, fahrenheitBtn);
   displayWeatherInfo.textContent = '';
   displayWeatherInfo.appendChild(div);
-};
+}
 
 function displayError(errorMessage) {
   displayWeatherInfo.textContent = errorMessage;
